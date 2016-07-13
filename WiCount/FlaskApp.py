62b01faf1da584baa5ed88373a01c87e214b7abc
Rakesh Lakshman <rakesh-muddaiah.lakshman@ucdconnect.ie>
2016-07-13 12:42:25 +0100

    Adding adduser and login html to jinja template

--- WiCount/FlaskApp.py
+++ WiCount/FlaskApp.py
@@ -27,7 +27,7 @@ def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return "Hello Boss!  <a href='/logout'>Logout</a>"
+        return render_template('lecturerapp.html')
  
 
 @app.route('/login', methods=['POST'])
