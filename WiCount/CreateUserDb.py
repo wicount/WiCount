882654fecb6a084b8bd05f84bb0cc9af899cd9f7
Rakesh Lakshman <rakesh-muddaiah.lakshman@ucdconnect.ie>
2016-08-04 14:38:29 +0100

    Adding comments

--- WiCount/CreateUserDb.py
+++ WiCount/CreateUserDb.py
@@ -5,13 +5,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 from passlib.hash import sha256_crypt
 
+# To connect to users database
 engine = create_engine('sqlite:///usersdb.db', echo=True)
 Base = declarative_base()
 
 class User(Base):
-  
+    # To create users table and define the field names    
     __tablename__ = "users"
-  
+    
     id = Column(Integer, primary_key=True)
     username = Column(String,unique=True)
     password = Column(String)
@@ -22,9 +23,11 @@ class User(Base):
         self.username = username
         self.email = email
         self.role = role
+        # Encrypt password
         self.password = sha256_crypt.encrypt(password)
         
     def verify(self, password):
+        # to verify the password during login
         return sha256_crypt.verify(password, self.password)  
   
 # create tables
