dc467ed5386d30858c1c4bbf9fe42d95fccf489d
Brendan <brendandwyer1@gmail.com>
2016-08-09 14:35:10 +0100

    Removed string operations from DB queries in statistics.

--- WiCount/statistics.py
+++ WiCount/statistics.py
@@ -1,8 +1,9 @@
 import sqlite3 as sql
 from sqlite3 import OperationalError
+import json
 
 
-def percentage_utilisation():
+def percentage_utilisation(room):
 
     con = sql.connect("wicount.sqlite3")
     cur = con.cursor()
@@ -10,10 +11,10 @@ def percentage_utilisation():
     sqlstring = 'SELECT DISTINCT room, (CAST (COUNT(room) AS FLOAT) / \
         (SELECT DISTINCT COUNT(room) FROM analytics) *100) AS PercentageUtilised \
         FROM analytics \
-        WHERE PredictedPercentage > 0'
+        WHERE PredictedPercentage > 0 and Room = ?'
 
     try:
-        percentage = cur.execute(sqlstring).fetchall()
+        percentage = cur.execute(sqlstring, (room,)).fetchall()
         con.commit()
         return percentage
 
@@ -34,47 +35,42 @@ def greater_lesser(numberpeople, comparison):
     else:
         operator = '<='
 
-    sqlstring = 'SELECT room, date\
+    sqlstring = "SELECT room, date\
     FROM analytics \
-    WHERE ' + str(numberpeople) + str(operator) + '((0.1 * PredictedPercentage) * Capacity)'
+    WHERE ? {} ((0.1 * PredictedPercentage) * Capacity)".format(operator)
 
-    try:
-        roomlist = cur.execute(sqlstring).fetchall()
-        con.commit()
-        return roomlist
+    # Have to use string operation because ">=" is an SQL parameter.
 
-    except OperationalError:
-        con.close()
-        return "An error occurred while fetching the requested rooms."
-
-    except Exception as e:
-        print(e)
+    roomlist = cur.execute(sqlstring, (numberpeople,)).fetchall()
+    con.commit()
+    return roomlist
 
 
-def list_occupancy_x(x):
+def list_occupancy_x(occupancy):
     # List rooms where occupancy = 0/50/100
     con = sql.connect("wicount.sqlite3")
     cur = con.cursor()
 
     # Select full rooms
-    sqlstring = 'SELECT room, date, Module \
+    sqlstring = 'SELECT room, date \
     from analytics \
-    WHERE PredictedPercentage ='+str(x)
+    WHERE PredictedPercentage = ?'
 
     try:
-        roomlist = cur.execute(sqlstring).fetchall()
+        roomlist = cur.execute(sqlstring, (occupancy,)).fetchall()
         con.commit()
         return roomlist
 
     except OperationalError:
         con.close()
-        return "An error occurred while fetching rooms of occupancy {}%.".format(x)
+        return "An error occurred while fetching rooms of occupancy {}%.".format(occupancy)
 
     except Exception as e:
         print(e)
 
 
-print(percentage_utilisation())
+print(percentage_utilisation("B-004"))
 print(greater_lesser(10, '>='))
-print('List occupancy:', list_occupancy_x(50))
+print('List occupancy:', list_occupancy_x("50"))
+
 
