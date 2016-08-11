e439e770aa146d309bb81d8413d30477cde0b92c
veldi_000 <velda.conaty@ucdconnect.ie>
2016-08-11 11:13:17 +0100

    trying google charts

---
+++ WiCount/statistics2.py
@@ -0,0 +1,108 @@
+import sqlite3 as sql
+from sqlite3 import OperationalError
+import json
+
+def getBuildings():
+    con = sql.connect("wicount.sqlite3")
+    con.row_factory = sql.Row # This enables column access by name: row['column_name'] 
+    cur = con.cursor()
+
+    rows = cur.execute('''SELECT DISTINCT(building) FROM room''').fetchall()
+
+    con.commit()
+    con.close()
+    print ("1: ", json.dumps( [dict(ix) for ix in rows] ))
+    return json.dumps( [dict(ix) for ix in rows] ) #CREATE JSON
+   
+def getPerUtal():
+    con = sql.connect("wicount.sqlite3")
+    con.row_factory = sql.Row # This enables column access by name: row['column_name'] 
+    cur = con.cursor()
+
+    rows = cur.execute('''SELECT  room_id, PredictedPercentage, count(*) as count, room From analytics GROUP BY room_id, PredictedPercentage''').fetchall()
+
+    con.commit()
+    con.close()
+    print ("1: ", json.dumps( [dict(ix) for ix in rows] ))
+    return json.dumps( [dict(ix) for ix in rows] ) #CREATE JSON
+    
+def percentage_utilisation(room):
+
+    con = sql.connect("wicount.sqlite3")
+    con.row_factory = sql.Row # Enables column access by name: row['column_name']
+
+    cur = con.cursor()
+
+    sqlstring = 'SELECT DISTINCT room, (CAST (COUNT(room) AS FLOAT) / \
+        (SELECT DISTINCT COUNT(room) FROM analytics) *100) AS PercentageUtilised \
+        FROM analytics \
+        WHERE PredictedPercentage > 0 and Room = ?'
+
+    try:
+        percentage = cur.execute(sqlstring, (room,)).fetchall()
+        con.commit()
+        return json.dumps([dict(ix) for ix in percentage])
+
+    except OperationalError:
+        con.close()
+        return "An error occurred while calculating percentage utilisation"
+    except Exception as e:
+        print(e)
+
+
+def greater_lesser(numberpeople, comparison):
+
+    con = sql.connect("wicount.sqlite3")
+    con.row_factory = sql.Row # Enables column access by name: row['column_name']
+
+    cur = con.cursor()
+
+
+    if comparison == '>=':
+        operator = '>='
+    else:
+        operator = '<='
+
+    sqlstring = "SELECT room, date\
+    FROM analytics \
+    WHERE ? {} ((0.1 * PredictedPercentage) * Capacity)".format(operator)
+
+    # Have to use string operation because ">=" is an SQL parameter.
+
+    roomlist = cur.execute(sqlstring, (numberpeople,)).fetchall()
+    con.commit()
+    return json.dumps([dict(ix) for ix in roomlist])
+
+
+def list_occupancy_x(occupancy):
+    # List rooms where occupancy = 0/50/100
+    con = sql.connect("wicount.sqlite3")
+    con.row_factory = sql.Row # Enables column access by name: row['column_name']
+
+    cur = con.cursor()
+
+    # Select full rooms
+    sqlstring = 'SELECT * \
+    from analytics \
+    WHERE PredictedPercentage = ?'
+
+    try:
+        roomlist = cur.execute(sqlstring, (occupancy,)).fetchall()
+        con.commit()
+        return json.dumps([dict(ix) for ix in roomlist])
+
+
+    except OperationalError:
+        con.close()
+        return "An error occurred while fetching rooms of occupancy {}%.".format(occupancy)
+
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    # for testing
+    print('Percentage utilisation JSON:\n', percentage_utilisation("B-004"))
+    print('Greater/lesser JSON:\n', greater_lesser(10, '>='))
+    print('List occupancy: JSON\n', list_occupancy_x("50"))
+
