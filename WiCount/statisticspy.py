0cfdf065ba8ae1d03e0c276acb71850d373c6aba
Brendan <brendandwyer1@gmail.com>
2016-08-15 20:01:56 +0100

    Updated percentage_utilisation() function
    Now returns JSON containing count of each PredictedPercentage value for
    each room.

--- WiCount/statisticspy.py
+++ WiCount/statisticspy.py
@@ -6,20 +6,25 @@ from dateutil.parser import parse
 from datetime import datetime, timedelta
 
 
-def percentage_utilisation(room):
+def percentage_utilisation():
 
     con = sql.connect("wicount.sqlite3")
     con.row_factory = sql.Row # Enables column access by name: row['column_name']
 
     cur = con.cursor()
 
-    sqlstring = 'SELECT DISTINCT room, (CAST (COUNT(room) AS FLOAT) / \
-        (SELECT DISTINCT COUNT(room) FROM analytics) *100) AS PercentageUtilised \
-        FROM analytics \
-        WHERE PredictedPercentage > 0 and Room = ?'
+    sqlstring = 'SELECT room_id, PredictedPercentage, count(*) as count, room \
+                From analytics \
+                GROUP BY room_id, PredictedPercentage \
+                Order by room_id'
+
+    # sqlstring = 'SELECT DISTINCT room, (CAST (COUNT(room) AS FLOAT) / \
+    #     (SELECT DISTINCT COUNT(room) FROM analytics) *100) AS PercentageUtilised \
+    #     FROM analytics \
+    #     WHERE PredictedPercentage > 0 and Room = ?'
 
     try:
-        percentage = cur.execute(sqlstring, (room,)).fetchall()
+        percentage = cur.execute(sqlstring).fetchall()
         con.commit()
         return json.dumps([dict(ix) for ix in percentage])
 
@@ -80,9 +85,79 @@ def list_occupancy_x(occupancy):
         print(e)
 
 
-if __name__ == '__main__':
-    # for testing
-    print('Percentage utilisation JSON:\n', percentage_utilisation("B-004"))
-    print('Greater/lesser JSON:\n', greater_lesser(10, '>='))
-    print('List occupancy: JSON\n', list_occupancy_x("50"))
+# if __name__ == '__main__':
+#     # for testing
+#     print('Percentage utilisation JSON:\n', percentage_utilisation("B-004"))
+#     print('Greater/lesser JSON:\n', greater_lesser(10, '>='))
+#     print('List occupancy: JSON\n', list_occupancy_x("50"))
 
+
+def roomUtilisation():
+    con = sql.connect("wicount.sqlite3")
+    con.row_factory = sql.Row
+    cur = con.cursor()
+
+    rows = cur.execute('''SELECT room, ROUND(predictions) as predictions FROM analytics GROUP BY room''').fetchall()
+
+    con.commit()
+    con.close()
+
+    return json.dumps( [dict(ix) for ix in rows],sort_keys=False)
+
+def timeUtilisation():
+    con = sql.connect("wicount.sqlite3")
+    con.row_factory = sql.Row
+    cur = con.cursor()
+
+    rows = cur.execute('''SELECT date, ROUND(predictions) as predictions FROM analytics GROUP BY date''').fetchall()
+
+    con.commit()
+    con.close()
+
+    return json.dumps( [dict(ix) for ix in rows],sort_keys=False)
+
+def dayUtilisation():
+    con = sql.connect("wicount.sqlite3")
+    con.row_factory = sql.Row
+    cur = con.cursor()
+
+    rows = cur.execute('''SELECT day, ROUND(predictions) as predictions FROM analytics GROUP BY day''').fetchall()
+
+    con.commit()
+    con.close()
+
+    return json.dumps( [dict(ix) for ix in rows],sort_keys=False)
+
+def occupnacyBasedOnTime():
+    return 0
+
+def occupnacyBasedOnDay():
+    return 0
+
+def percentgeOccupiedRoom():
+    con = sql.connect("wicount.sqlite3")
+    con.row_factory = sql.Row
+    cur = con.cursor()
+
+    rows = cur.execute('''SELECT room FROM analytics GROUP BY room''').fetchall()
+
+    con.commit()
+    con.close()
+
+    return json.dumps( [dict(ix) for ix in rows],sort_keys=False)
+
+def percentgeOccupiedTime():
+    return 0
+
+def percentgeOccupiedDay():
+    return 0
+
+def listEmptyRoom():
+    return 0
+
+def listFullRoom():
+    return 0
+
+if __name__ == '__main__':
+
+    print(percentgeOccupiedRoom())
