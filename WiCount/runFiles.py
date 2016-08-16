adfc96f05a6b8c0aa43cc5bdc0b2d20a8107b30a
harneyp2 <patrick.harney+git@gmail.com>
2016-08-16 23:13:13 +0100

    Code Review and Commenting

--- WiCount/runFiles.py
+++ WiCount/runFiles.py
@@ -1,3 +1,4 @@
+#Set path for libaries on server
 #import sys
 #sys.path.insert(0, "/home/student/anaconda3/lib/python3.4/site-packages")
 
@@ -9,7 +10,7 @@ import wicount
 import db
 import DataAnalysis
 
-
+#Connect to the database
 con = db.get_connection()
 c=con.cursor()
 # Create all the database tables
@@ -18,6 +19,8 @@ MakeLogDataDB.main()
 makeSurveyDB.main()
 makeTimeTableDB.main()
 
+#Run the analytics algorithm
 DataAnalysis.main()
 
+#Close the database connection
 con.close() 
